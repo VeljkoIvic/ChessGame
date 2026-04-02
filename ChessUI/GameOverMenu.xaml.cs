3f9cca6c0620bcb0142108d8867706223384b27f
Ivic Veljko <ivic-veljko@hotmail.ch>
2026-04-02 23:18:17 +0200

    Add GameOverMenu UserControl with UI and event handlers
    Introduced a new GameOverMenu UserControl with XAML layout for displaying winner and reason texts, plus "RESTART" and "EXIT" buttons. Added code-behind with empty event handlers for button clicks, prepared for further logic implementation.

---
+++ ChessUI/GameOverMenu.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ChessUI;
+
+/// <summary>
+/// Interaction logic for GameOverMenu.xaml
+/// </summary>
+public partial class GameOverMenu : UserControl
+{
+    public GameOverMenu()
+    {
+        InitializeComponent();
+    }
+
+    private void Restart_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Exit_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+}
